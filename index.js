cba9aeab8c8aaccc6a2406de3bbb38805dd528c0
adrigunawan <adrigunawan32@gmail.com>
2023-01-12 14:12:16 +0700

    test-api

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
 const app = express();
-// const router = require("./router");
+const router = require("./router");
 
-app.all("/login", (req, res) => {
-  console.log("Just got a request!");
-  res.send("Halaman Login");
-});
+app.use(router);
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
